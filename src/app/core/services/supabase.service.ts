96e84230e9c7a8dc85742cf2a477e52b454514c4
Pablo Perea Campos <pablopereac02@gmail.com>
2026-01-22 12:58:44 +0100

    refactor(supabase): métodos iniciar sesión y registro

--- src/app/core/services/supabase.service.ts
+++ src/app/core/services/supabase.service.ts
@@ -4,24 +4,34 @@ import { environment } from '../../../environments/environment.development';
 import { Producto } from '../models/producto.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class SupabaseService {
-  private supabase: SupabaseClient;
+    private supabase: SupabaseClient;
 
-  constructor() {
-    this.supabase = createClient(environment.supabase.url, environment.supabase.key);
-  }
-
-  async getProductos(): Promise<Producto[]> {
-    const { data, error } = await this.supabase
-      .from('productos')
-      .select('*');
-
-    if (error) {
-      console.error('Error cargando productos:', error);
-      return [];
+    constructor() {
+        this.supabase = createClient(environment.supabase.url, environment.supabase.key);
     }
-    return data as Producto[] || [];
-  }
+
+    async signIn(email: string, password: string): Promise<{ error: any }> {
+        const { error } = await this.supabase.auth.signInWithPassword({ email, password });
+        return { error };
+    }
+
+    async signUp(email: string, password: string): Promise<{ error: any }> {
+        const { error } = await this.supabase.auth.signUp({ email, password });
+        return { error };
+    }
+
+    async getProductos(): Promise<Producto[]> {
+        const { data, error } = await this.supabase
+            .from('productos')
+            .select('*');
+
+        if (error) {
+            console.error('Error cargando productos:', error);
+            return [];
+        }
+        return data as Producto[] || [];
+    }
 }
