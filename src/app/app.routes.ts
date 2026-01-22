8109042dbf9ab7ca3c3651ec40d9f0a874351aa0
Pablo Perea Campos <pablopereac02@gmail.com>
2026-01-22 12:58:25 +0100

    feat(catalog): añadido el componente catálogo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login';
+import { CatalogComponent } from './features/catalog/catalog'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CatalogComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '' } // Cualquier ruta desconocida va al Home
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,31 +1,10 @@
-import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SupabaseService } from './core/services/supabase.service';
-import { Producto } from './core/models/producto.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`
 })
-export class App implements OnInit {
-  private supabaseService = inject(SupabaseService);
-  private cdr = inject(ChangeDetectorRef);
-  
-  productos: Producto[] = [];
-
-  async ngOnInit() {
-    console.log('Solicitando datos...');
-    
-    // Se piden los datos
-    this.productos = await this.supabaseService.getProductos();
-    
-    console.log('Datos llegaron, forzando actualización de vista...');
-    
-    // Obligar a Angular a actualizar la vista
-    this.cdr.detectChanges(); 
-  }
-}
+export class App {}
