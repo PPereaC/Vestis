656b594522256500ef912914c978c74721fc4184
Pablo Perea Campos <pablo.perea@alumnojoyfe.iepgroup.es>
2026-01-22 19:20:27 +0100

    feat(home-page): implementación de listado de productos con el ProductService

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { ProductCard } from '@products/components/product-card/product-card';
-//import { ProductCard } from '../../../products/components/product-card/product-card';
+import { ProductService } from '@products/services/product.service';
+import { Product } from '@products/interfaces/product.interface';
 
 @Component({
   selector: 'app-home-page',
+  standalone: true,
   imports: [ProductCard],
-  templateUrl: './home-page.html',
+  template: `
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      @for (item of products(); track item.id) {
+        <product-card [product]="item" />
+      }
+    </div>
+  `
 })
-export class HomePage { }
+export class HomePage implements OnInit {
+  private productService = inject(ProductService);
+
+  products = signal<Product[]>([]); 
+
+  async ngOnInit() {
+    const data = await this.productService.getProducts();
+    this.products.set(data);
+  }
+}
