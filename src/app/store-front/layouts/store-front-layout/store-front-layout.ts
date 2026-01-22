f88c5fbcb6c57c0a6fcb7feb12ecf2de8aeeb656
Pablo Perea Campos <pablo.perea@alumnojoyfe.iepgroup.es>
2026-01-22 16:35:59 +0100

    feat(store-front): añadido el layout del front de la tienda y las rutas con páginas

---
+++ src/app/store-front/layouts/store-front-layout/store-front-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-store-front-layout',
+  imports: [RouterOutlet],
+  templateUrl: './store-front-layout.html',
+})
+export class StoreFrontLayout { }
