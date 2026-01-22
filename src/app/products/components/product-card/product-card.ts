6397a99a8e9d3b65fc76c59e15b0d4775473aeb1
Pablo Perea Campos <pablo.perea@alumnojoyfe.iepgroup.es>
2026-01-22 19:19:06 +0100

    feat(product-card): añadido input en la tarjeta de producto para mandar los datos de este

--- src/app/products/components/product-card/product-card.ts
+++ src/app/products/components/product-card/product-card.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { Product } from '@products/interfaces/product.interface';
 
 @Component({
   selector: 'product-card',
   imports: [RouterLink],
   templateUrl: './product-card.html',
 })
-export class ProductCard { }
+export class ProductCard {
+  product = input.required<Product>();
+}
