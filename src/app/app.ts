802ef66e4aa6ba0561b3ea4b6a5192da7a7dd958
Pablo Perea Campos <pablopereac02@gmail.com>
2026-01-22 12:06:14 +0100

    feat(app): implementado la obtención de productos y actualización de la vista al iniciar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SupabaseService } from './core/services/supabase.service';
+import { Producto } from './core/models/producto.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('adidaw');
+export class App implements OnInit {
+  private supabaseService = inject(SupabaseService);
+  private cdr = inject(ChangeDetectorRef);
+  
+  productos: Producto[] = [];
+
+  async ngOnInit() {
+    console.log('Solicitando datos...');
+    
+    // Se piden los datos
+    this.productos = await this.supabaseService.getProductos();
+    
+    console.log('Datos llegaron, forzando actualización de vista...');
+    
+    // Obligar a Angular a actualizar la vista
+    this.cdr.detectChanges(); 
+  }
 }
