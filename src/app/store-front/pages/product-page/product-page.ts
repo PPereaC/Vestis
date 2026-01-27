3139024f9c1b105cfe72834b80761d986c179ca9
Pablo Perea Campos <pablo.perea@alumnojoyfe.iepgroup.es>
2026-01-27 17:04:54 +0100

    feat(product-page): implementado boceto de mostrar imágenes en página de producto

--- src/app/store-front/pages/product-page/product-page.ts
+++ src/app/store-front/pages/product-page/product-page.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './product-page.html',
 })
-export class ProductPage { }
+export class ProductPage {
+  images = [
+    { color: 'bg-primary' },
+    { color: 'bg-secondary' },
+    { color: 'bg-accent' },
+    { color: 'bg-neutral' },
+  ];
+
+  get imageClass() {
+    return this.images.length === 4 ? 'flex-1 min-h-0' : 'h-[150px]';
+  }
+
+}
