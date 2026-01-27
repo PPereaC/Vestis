221138ff7b2225b111e82b283741fd16a591ffaa
Pablo Perea Campos <pablopereac02@gmail.com>
2026-01-27 23:14:32 +0100

    feat(product-interface): actualizada la interfaz Producto con la nueva estructura de la base de datos

--- src/app/products/interfaces/product.interface.ts
+++ src/app/products/interfaces/product.interface.ts
@@ -1,11 +1,16 @@
 export interface Product {
-  id: string;
-  nombre: string;
-  descripcion?: string;
-  marca?: string;
-  categoria?: string;
-  precio_base?: number;
-  imagen_url?: string;
-  created_at?: string;
-  cantidadVariantes?: number;
+    id: number;
+    nombre: string;
+    descripcion?: string;
+    precio_base: number;
+    precio_oferta?: number;
+    tiene_oferta: boolean;
+    imagen_principal_url?: string;
+    categoria?: string;
+    marca?: string;
+    color_default?: string;
+    genero?: 'Hombre' | 'Mujer' | 'Unisex' | 'Niños';
+    fecha_creacion: string;
+    cantidadVariantes?: number;
 }
+
