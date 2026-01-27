c4ff7f6530ffa8079ba7afa6c1f7ac86c3f62d38
Pablo Perea Campos <pablo.perea@alumnojoyfe.iepgroup.es>
2026-01-27 18:45:31 +0100

    feat(product-page): información producto (sin finalizar)

--- src/app/store-front/pages/product-page/product-page.ts
+++ src/app/store-front/pages/product-page/product-page.ts
@@ -1,18 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { NgClass } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { ServicioProductos } from '@products/services/product.service';
+import { Product } from '@products/interfaces/product.interface';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-page',
-  imports: [NgClass],
+  imports: [NgClass, CurrencyPipe],
   templateUrl: './product-page.html',
 })
 export class ProductPage {
-  images = [
-    { color: 'bg-primary' },
-    { color: 'bg-secondary' },
-    { color: 'bg-accent' },
-    { color: 'bg-neutral' },
-  ];
+
+  private servicioProductos = inject(ServicioProductos);
+  private route = inject(ActivatedRoute);
+
+  productoId = signal<string>('');
+  producto = signal<Product | null>(null);
+  imagenesProducto = signal<string[]>([]);
+  portadaProducto = signal<string | null>(null);
+
+  async ngOnInit() {
+
+    this.productoId.set(this.route.snapshot.paramMap.get('id') || '');
+    
+    const data = await this.servicioProductos.obtenerProductoPorId(this.productoId());
+    this.producto.set(data);
+
+    const imagenes = await this.servicioProductos.obtenerImagemesDeProducto(this.productoId());
+    this.imagenesProducto.set(imagenes);
+
+    const portada = await this.servicioProductos.obtenerPortadaProducto(this.productoId());
+    this.portadaProducto.set(portada);
+
+  }
+
+  images = this.imagenesProducto;
 
   get imageClass() {
     return this.images.length === 4 ? 'flex-1 min-h-0' : 'h-[150px]';
