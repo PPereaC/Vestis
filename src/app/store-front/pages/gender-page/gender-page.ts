f867e91239632913aac0b450b2d5dc320700abe9
Pablo <pereacampospablo@gmail.com>
2026-02-01 18:49:59 +0100

    feat(gender-page): cambiada la vista de ropa a la página de género en vez de en home screen

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ProductCard } from '@products/components/product-card/product-card';
+import { Product } from '@products/interfaces/product.interface';
+import { ServicioProductos } from '@products/services/product.service';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './gender-page.html',
 })
-export class GenderPage { }
+export class GenderPage implements OnInit {
+
+  private servicioProductos = inject(ServicioProductos);
+
+  productos = signal<Product[]>([]); 
+
+  async ngOnInit() {
+    const data = await this.servicioProductos.obtenerProductos();
+    this.productos.set(data);
+  }
+
+}
--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,21 +1,11 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
-import { ProductCard } from '@products/components/product-card/product-card';
-import { ServicioProductos } from '@products/services/product.service';
-import { Product } from '@products/interfaces/product.interface';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ProductCard],
+  imports: [],
   templateUrl: './home-page.html',
 })
-export class HomePage implements OnInit {
-  private servicioProductos = inject(ServicioProductos);
-
-  productos = signal<Product[]>([]); 
-
-  async ngOnInit() {
-    const data = await this.servicioProductos.obtenerProductos();
-    this.productos.set(data);
-  }
+export class HomePage {
+  
 }
