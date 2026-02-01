05b650da368bbc63af7947e9979601e070e15883
Pablo <pereacampospablo@gmail.com>
2026-02-01 18:17:14 +0100

    feat(navbar): nuevo diseño de navbar más moderno para móvil y escritorio

--- src/app/store-front/components/front-navbar/front-navbar.ts
+++ src/app/store-front/components/front-navbar/front-navbar.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'front-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    LucideAngularModule
+  ],
   templateUrl: './front-navbar.html',
 })
-export class FrontNavbar { }
+export class FrontNavbar {
+  
+}
