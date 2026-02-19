55489ca54b956ce23658413be7e3ada92c3fe40e
Pablo Perea Campos <pablopereac02@gmail.com>
2026-02-19 16:38:44 +0100

    feat(routes): añadida ruta para la página de inicio de sesión

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './store-front/pages/login-page/login-page';
 
 export const routes: Routes = [
 
+    {
+        path: 'login',
+        component: LoginPage
+    },
     {
         path: '',
         loadChildren: () => import('./store-front/store-front-routes')
