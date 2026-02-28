0972328253bae8aa845a9c187c1cdd7feecec7ab
Pablo <pereacampospablo@gmail.com>
2026-02-28 14:30:43 +0100

    feat(cart): añadida página de carrito con navegación desde navbar y dropdown

---
+++ src/app/store-front/pages/cart-page/cart-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-cart-page',
+  imports: [RouterLink],
+  templateUrl: './cart-page.html',
+})
+export class CartPage {
+
+}
--- src/app/store-front/store-front-routes.ts
+++ src/app/store-front/store-front-routes.ts
@@ -4,6 +4,7 @@ import { HomePage } from "./pages/home-page/home-page";
 import { GenderPage } from "./pages/gender-page/gender-page";
 import { ProductPage } from "./pages/product-page/product-page";
 import { NotFoundPage } from "./pages/not-found-page/not-found-page";
+import { CartPage } from "./pages/cart-page/cart-page";
 
 
 export const storeFrontRoutes: Routes = [
@@ -22,6 +23,10 @@ export const storeFrontRoutes: Routes = [
             {
                 path: 'producto/:id',
                 component: ProductPage
+            },
+            {
+                path: 'carrito',
+                component: CartPage
             },
             {
                 path: '**',
